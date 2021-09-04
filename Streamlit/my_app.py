46c9ba59cedcd9620db854c8f35af390f24a4ec2
Gustavo R Santos <50956352+gurezende@users.noreply.github.com>
2021-09-04 19:55:02 -0400

    Update my_app.py

--- Streamlit/my_app.py
+++ Streamlit/my_app.py
@@ -48,6 +48,12 @@ st.sidebar.text('By City')
 filter_city = st.sidebar.multiselect('Filter By City', options=city, default=city)
 df = df.query('city in @filter_city')
 
+
+# About Me
+st.text('Demonstration App created with the toy dataset *Tips* from seaborn')
+st.text('App created by Gustavo R Santos')
+st.markdown('[Check out my blog on Medium](https://medium.com/gustavorsantos)')
+
 #-----------------------------------------------------------------------
 
 # Title
